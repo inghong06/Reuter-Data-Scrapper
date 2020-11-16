7f577b460d6cd2b0589e04a68cb069009e74c52f
inghong06 <63499402+inghong06@users.noreply.github.com>
2020-11-16 22:15:37 +0800

    Update reuter_data_scrapper.py

--- reuter_data_scrapper.py
+++ reuter_data_scrapper.py
@@ -1,4 +1,4 @@
-def reuter_data(stock, data, period, new_name, folder_name):
+def reuter_data(stock, data, period, folder_name, csv_name):
     url = "https://www.reuters.com/companies/" + stock + "/financials/" + data + "-" + period
     print(url)
     read_data = urlopen(url).read()
@@ -8,36 +8,80 @@ def reuter_data(stock, data, period, new_name, folder_name):
     final_list = []
     index = 0
 
-    while index < len(table): # iteration of loop depends on number of rows in table
-        temp_list = [] # Start loop with an empty list
-        for line in table[index]: # iteration of loop depends on number of item in a row (or number of column)
-            temp_list.append(line.text) # append all item in current row in a list
-        final_list.append(temp_list) # append temp list and forms a row
-        print(final_list[index])
-        index += 1 # index increment to go to the next row
-    file_name = new_name + "_" + data + "_" + period + ".csv" # determine file name after data frame is completed
-    print("Storing data to", file_name)
-    new_file_name = os.path.join(folder_name, file_name) # path for new file
-    df = pd.DataFrame(final_list[1:]) # store data into data frame starting from row 1 until the end
-    df.to_csv(new_file_name, index=False) # store data frame in .csv form
-    print(file_name, "saved to", folder_name)
+    while index < len(table):  # iteration of loop depends on number of rows in table
+        temp_list = []  # Start loop with an empty list
+        for line in table[index]:  # iteration of loop depends on number of item in a row (or number of column)
+            temp_list.append(line.text)  # append all item in current row in a list
+        final_list.append(temp_list)  # append temp list and forms a row
+        index += 1  # index increment to go to the next row
+    df = pd.DataFrame(final_list[1:])  # store data into data frame starting from row 1 until the end
+    print(df)
+    print("Storing data to", folder_name)
+    new_file_name = os.path.join(folder_name, csv_name)  # path for new file
+    df.to_csv(new_file_name, index=False)  # store data frame in .csv form
+    print(csv_name, "saved to", folder_name)
 
-def company_name(stock):
-    stock_name=[]
-    url = "https://www.reuters.com/companies/" + stock + "/financials/"
-    read_data = urlopen(url).read()
-    soup = BeautifulSoup(read_data, 'html.parser')
-    item = soup.find_all("h1") # find header of the url
-    for i in item:
-        stock_name = (i.text)
-    return stock_name
 
-def create_folder():
+class FindStock:
+
+    def __init__(self, stock):
+        stock = stock.replace(" ", "+")
+        url = "https://www.reuters.com/search/news?blob="
+        search = url + stock
+        self.stock = stock
+        self.url = search
+        read_data = urlopen(search).read()
+        self.soup = BeautifulSoup(read_data, 'html.parser')
+        self.result_list = []
+
+
+
+    def company_name(self):
+        result_list =[]
+        search_results = self.soup.find_all("div", class_='search-stock-ticker')
+        for i in search_results:
+            result_list.append(i.text)
+
+        return result_list[0]
+
+    def exchange(self):
+        result_list = []
+        search_results = self.soup.find_all("div", class_='search-stock-exchange')
+        for i in search_results:
+            result_list.append(i.text.replace("\r\n    ", ""))
+        return result_list[0]
+
+    def ticker(self):
+        ticker = self.company_name().split(" ")
+        ticker = ticker[-1].replace("(","")
+        ticker = ticker.replace(")", "")
+        return ticker
+
+
+    def company_name_without_ticker(self):
+        split = self.company_name().split(" ")
+        comp_name = self.company_name().replace(split[-1],"")
+        return comp_name[:-1]
+
+    def file_name(self):
+        file_name = self.company_name_without_ticker().replace(" ","_")
+        file_name = file_name.replace("-", "_")
+        return file_name
+
+
+def create_folder(company_name):
+    folder_name = os.path.join(target, company_name)
     try:
         os.mkdir(folder_name)
         print("Create folder for", folder_name)
     except FileExistsError:
         print(stock, "folder already exist")
+
+def stock_info(name, exchange, ticker):
+    print("company name is ", name)
+    print("stock exchange: ", exchange)
+    print("stock ticker: ", ticker)
+
 
 import os, re, os.path
 import pandas as pd
@@ -46,23 +90,33 @@ from bs4 import BeautifulSoup
 
 target = "D:\\Stocks_Financial_Data\\Reuter\\"
 
-stock = input("Please enter stock: ")
-stock_name = company_name(stock)
-print("Scraping data for", stock_name)
-folder_name = os.path.join(target, stock_name)
+# stock = input("Please enter stock: ")
+stock = "berkshire"
+stock = FindStock(stock)
 
-create_folder()
+company_name = stock.company_name_without_ticker()
+exchange = stock.exchange()
+ticker = stock.ticker()
+file_name = stock.file_name()
 
-new_name = stock_name.replace(" " , "_")
-new_name = new_name.replace("-" , "_") # new_name is name used for naming files later
+stock_info(company_name, exchange, ticker)
+
+
+print("Scraping data for", company_name)
+
+folder_name = os.path.join(target, company_name)
+try:
+    os.mkdir(folder_name)
+    print("Create folder for", folder_name)
+except FileExistsError:
+    print(folder_name, "already exist")
+
+
 data_list = ["income-statement", "balance-sheet", "cash-flow"]
 period_list = ["annual", "quarterly"]
 
 
-
 for data in data_list:
     for period in period_list:
-        reuter_data(stock, data, period, new_name, folder_name)
-
-
-
+        csv_name = file_name + "_" + data + "_" + period + ".csv"  # determine file name after data frame is completed
+        reuter_data(ticker, data, period, folder_name, csv_name)
